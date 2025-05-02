62d353c08bbfa65f925228bd397204f2a9ce9104
Dejnyyy <danslaby@email.cz>
2025-05-02 10:33:41 -0400

    jeste tak

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      // adjust these globs to wherever you use Tailwind classes
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        // add custom colors, spacing, fonts, etc. here
-      },
-    },
-    plugins: [
-      // e.g. require('@tailwindcss/forms'),
-    ],
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: { extend: {} },
+    plugins: [],
   };
   
